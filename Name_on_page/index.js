9ca5789d35d507a98014ce387c3e3f0ea8a23345
temitope-israel <52775777+temitope-israel@users.noreply.github.com>
2021-08-20 10:55:46 +0100

    Final Commit

---
+++ Name_on_page/index.js
@@ -0,0 +1,4 @@
+const fullName = document.getElementById("name");
+console.log(fullName);
+fullName.innerHTML = "My name is Jesuniyi Temitope Israel";
+console.log("My name is Jesuniyi Temitope Israel");
